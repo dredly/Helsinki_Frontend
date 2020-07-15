31cb7e370f27f80e703fbaeddc6fc42364e059d1
dredly <dredly@gmail.com>
2020-07-15 14:51:32 +0100

    Added getInfo function

--- src/services/persons.js
+++ src/services/persons.js
@@ -6,6 +6,11 @@ const getAll = () => {
 	return request.then(response => response.data)
 }
 
+const getInfo = () => {
+	const request = axios.get('http://localhost:3001/api/persons')
+	return request.then(response => response.data)
+}
+
 const create = newObject => {
 	const request = axios.post(baseUrl, newObject)
 	return request.then(response => response.data)
